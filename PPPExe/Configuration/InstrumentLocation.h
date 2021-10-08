372bb10dd126d66067f4be641a482875a3f15f44
Santiago Arellano <sarellanos@gmail.com>
2021-10-08 16:52:41 +0200

    Added CInstrumentLocation to InstrumentLocation.cpp to set all initial values of instrument setup to 0.

--- PPPExe/Configuration/InstrumentLocation.h
+++ PPPExe/Configuration/InstrumentLocation.h
@@ -19,6 +19,7 @@ namespace Configuration
     class CInstrumentLocation
     {
     public:
+		CInstrumentLocation();
 
         // -----------------------------------------
         // -------------- PUBLIC DATA --------------
